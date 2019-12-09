8e6aaf2f010f1f9c8dd74511a0605bc9dd8744e3
Ramiz Raja <informramiz@gmail.com>
2019-12-09 17:48:17 +0500

    feat: Add pin drop logic

--- VirtualTourist/VirtualTourist/Controller/MapViewController.swift
+++ VirtualTourist/VirtualTourist/Controller/MapViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        addMapGesture()
     }
+    
+    private func addMapGesture() {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(addMarker(gesture:)))
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    fileprivate func addMapMarker(_ pinCoordinates: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = pinCoordinates
+        mapView.addAnnotation(annotation)
+    }
+    
+    @objc
+    private func addMarker(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            // gesture accepted
+            let pinPoint = gesture.location(in: mapView)
+            let pinCoordinates = mapView.convert(pinPoint, toCoordinateFrom: mapView)
+            addMapMarker(pinCoordinates)
+        }
+    }
+}
 
-
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let pinIdentifier = "pin"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: pinIdentifier) as? MKPinAnnotationView
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: pinIdentifier)
+            annotationView?.animatesDrop = true
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
 }
 
