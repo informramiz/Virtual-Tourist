c1ea28a59d9e74fce1d91a3f97473fa25707e080
Ramiz Raja <informramiz@gmail.com>
2019-12-11 01:00:10 +0500

    feat: Write custom collection cell

---
+++ VirtualTourist/VirtualTourist/View/Custom Cells/PhotoCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionCell.swift
+//  VirtualTourist
+//
+//  Created by Apple on 11/12/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+
+
+
+
+
+import Foundation
+import UIKit
+
+class PhotoCollectionCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
