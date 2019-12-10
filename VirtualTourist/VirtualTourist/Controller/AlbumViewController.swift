2a5b4654ef00e6bc0ef3b16e14729cbef26b532d
Ramiz Raja <informramiz@gmail.com>
2019-12-10 18:06:49 +0500

    feat: Fully connect album UI with controller

--- VirtualTourist/VirtualTourist/Controller/AlbumViewController.swift
+++ VirtualTourist/VirtualTourist/Controller/AlbumViewController.swift
@@ -14,22 +14,17 @@ class AlbumViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var noImagesLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var newCollectionButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        noImagesLabel.isHidden = true
+        newCollectionButton.isEnabled = false
+        mapView.isUserInteractionEnabled = false
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func newCollection(_ sender: Any) {
+        
     }
-    */
-
 }
