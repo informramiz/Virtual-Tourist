6042f11e791532b712c3da77f9a3a40b3ffcaa11
Ramiz Raja <informramiz@gmail.com>
2019-12-11 21:20:07 +0500

    refactor: Improve Collection view update logic

--- VirtualTourist/VirtualTourist/View/Custom Views/MyUICollectionView.swift
+++ VirtualTourist/VirtualTourist/View/Custom Views/MyUICollectionView.swift
@@ -14,47 +14,104 @@ import CoreData
 class MyUICollectionView: UICollectionView {
     //because CollectionView does not have beginUpdates() and endUpdates() method like tableview so
     //we need to implement that functionality ourself. Below are variables to keep track of changes
-    private var itemChanges = [NSFetchedResultsChangeType: IndexPath]()
-    private var sectionChanges = [NSFetchedResultsChangeType: IndexSet]()
+    private let changeKeeper: ChangeKeeper = ChangeKeeper()
+    
+    struct ItemChangeKeeper {
+        var insertedItems = [IndexPath]()
+        var deletedItems = [IndexPath]()
+        var updatedItems = [IndexPath]()
+        var movedItems = [IndexPath]()
+    }
+    
+    struct SectionChangeKeeper {
+        var insertedSections = [IndexSet]()
+        var deletedSections = [IndexSet]()
+    }
+    
+    class ChangeKeeper {
+        var itemChangeKeeper = [NSFetchedResultsChangeType: [IndexPath]]()
+        var sectionChangeKeeper = [NSFetchedResultsChangeType: [IndexSet]]()
+        
+        func addSectionChange(type: NSFetchedResultsChangeType, sectionIndex: Int) {
+            var array = sectionChangeKeeper[type] ?? []
+            array.append(IndexSet(integer: sectionIndex))
+            sectionChangeKeeper[type] = array
+            
+            clearItemsFor(section: sectionIndex, sectionType: type)
+        }
+        
+        func addItemChange(type: NSFetchedResultsChangeType, indexPath: IndexPath?, newIndexPath: IndexPath?) {
+            var changes = itemChangeKeeper[type] ?? []
+            switch type {
+            case .insert:
+                changes.append(newIndexPath!)
+                break
+            case .delete, .update:
+                changes.append(indexPath!)
+                break
+            default:
+                break
+            }
+            itemChangeKeeper[type] = changes
+        }
+        
+        private func clearItemsFor(section: Int, sectionType: NSFetchedResultsChangeType) {
+            switch sectionType {
+            case .insert:
+                clearItemsFor(section: section, itemChangeType: .insert)
+            case .delete:
+                clearItemsFor(section: section, itemChangeType: .delete)
+                clearItemsFor(section: section, itemChangeType: .update)
+            default:
+                break
+            }
+        }
+        
+        private func clearItemsFor(section: Int, itemChangeType: NSFetchedResultsChangeType) {
+            let items = itemChangeKeeper[itemChangeType] ?? []
+            itemChangeKeeper[itemChangeType] = items.filter { (indexPath) -> Bool in
+                return indexPath.section != section
+            }
+        }
+        
+        func clearChanges() {
+            itemChangeKeeper.removeAll()
+            sectionChangeKeeper.removeAll()
+        }
+    }
     
     func itemDidChange(indexPath: IndexPath?, type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         //keep track of all items related changes in self.itemChanges dictionary so that we can apply them in batch
-        switch type {
-        case .insert:
-            itemChanges[type] = newIndexPath
-            break
-        case .delete, .update:
-            itemChanges[type] = indexPath
-            break
-        default:
-            reloadData()
-            break
-        }
+        changeKeeper.addItemChange(type: type, indexPath: indexPath, newIndexPath: newIndexPath)
     }
     
     func sectionDidChange(sectionIndex: Int, type: NSFetchedResultsChangeType) {
         //keep track of section related changes so that we can apply them in batch
-        let indexSet = IndexSet(integer: sectionIndex)
-        sectionChanges[type] = indexSet
+        changeKeeper.addSectionChange(type: type, sectionIndex: sectionIndex)
     }
     
     func beginUpdates() {
         //BeginUpdates: new changes are about to begin so discard outdated changes that we have already applied
-        itemChanges.removeAll()
-        sectionChanges.removeAll()
+        changeKeeper.clearChanges()
     }
     
     func endUpdates() {
         //EndUpdates: Now all changes are triggered, let's apply all changes in batch
         performBatchUpdates({
             //apply section related changes
-            for (type, indexSet) in sectionChanges {
-                applySectionChange(type, indexSet)
+            for (type, indexSets) in changeKeeper.sectionChangeKeeper {
+                for indexSet in indexSets {
+                    applySectionChange(type, indexSet)
+                }
             }
             
+            
+            
             //apply items changes
-            for (type, indexPath) in itemChanges {
-                applyItemChange(type, indexPath)
+            for (type, indexPaths) in changeKeeper.itemChangeKeeper {
+                for indexPath in indexPaths {
+                    applyItemChange(type, indexPath)
+                }
             }
         }, completion: nil)
     }
